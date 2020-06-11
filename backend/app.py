8515b5d42c1fe681270edd42a4939be3a09a37e2
BeratKARATAS53 <b21527142@cs.hacettepe.edu.tr>
2020-06-11 16:38:48 +0300

    Updated

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_cors import CORS
-from flask import request
-from flask import jsonify
+from flask import request, jsonify, make_response
 from flask import render_template
-from flask import Blueprint
 
 import os
 import sys
+from io import StringIO
+    
 import tensorflow as tf
 import pdb
 import pickle
@@ -17,41 +17,56 @@ from pathlib import Path
 from keras.preprocessing.sequence import pad_sequences
 
 from emotion.pythonFiles import emotionModel
-from emotion.pythonFiles import preprocess
+from emotion.nlp import preprocess
 
 
 app = Flask(__name__)
 CORS(app)
 
-
+tokenizer_path = Path('./emotion/datasets/sentiment_analysis/tokenizer.pickle').resolve()
+with tokenizer_path.open('rb') as file:
+    tokenizer = pickle.load(file)
+    
 def get_test_data():
-    data_path = Path('../test.csv').resolve()
+    data_path = Path('./emotion/test.csv').resolve()
     data = pd.read_csv(data_path, header=None, names=['text'])
     return data
 
+def string_to_pandas(sentence):
+    three = "\"\"\""
+    sentence = three + sentence + three
+    data = StringIO(sentence)
+    df = pd.read_csv(data, header=None, names=['text'])
+    return df
+
 def get_encoder():
-    encoder_path = Path('./models/emotion_recognition/encoder.pickle').resolve()
+    encoder_path = Path('./emotion/models/emotion_recognition/encoder.pickle').resolve()
     with encoder_path.open('rb') as file:
         encoder = pickle.load(file)
     return encoder
 
-def preprocess_data():
-    cleaned_data = preprocess(data.text)
+def preprocess_data(sentence):
+    sentence = string_to_pandas(sentence)
+    cleaned_data = preprocess(sentence.text)
     sequences = [text.split() for text in cleaned_data]
     list_tokenized = tokenizer.texts_to_sequences(sequences)
     x_data = pad_sequences(list_tokenized, maxlen=100)
     return x_data
 
-def predict():
-    x_data = preprocess_data()
+def predictEmotion(sentence):
+    input_length = 100
+    model = emotionModel.emotionModel(tokenizer, input_length)
+    model_weights_path = Path('./emotion/models/emotion_recognition/model_weights.h5').resolve()
+    model.load_weights(model_weights_path.as_posix())
+    
+    x_data = preprocess_data(sentence)
     y_pred = model.predict(x_data)
+    
+    result = {}
     for index, value in enumerate(np.sum(y_pred, axis=0) / len(y_pred)):
-        print(encoder.classes_[index] + ": " + str(value))
-    y_pred_argmax = y_pred.argmax(axis=1)
-    data_len = len(y_pred_argmax)
-    for index, value in enumerate(np.unique(y_pred_argmax)):
-        print(encoder.classes_[index] + ": " + str(len(y_pred_argmax[y_pred_argmax == value]) / data_len))
-
+        result[encoder.classes_[index]] =value
+    
+    return result
 
 @app.route('/')
 def home():
@@ -75,27 +90,18 @@ def predict():
 
 @app.route('/predictModel', methods=['POST'])
 def predictModel():
-    x_data = preprocess_data()
-    y_pred = model.predict(x_data)
-    for index, value in enumerate(np.sum(y_pred, axis=0) / len(y_pred)):
-        print(encoder.classes_[index] + ": " + str(value))
-    y_pred_argmax = y_pred.argmax(axis=1)
-    data_len = len(y_pred_argmax)
-    for index, value in enumerate(np.unique(y_pred_argmax)):
-        print(encoder.classes_[index] + ": " + str(len(y_pred_argmax[y_pred_argmax == value]) / data_len))
+    sentence = request.get_json()
+    sentence = sentence["sentence"]
+    result = predictEmotion(sentence)
+    
+    maximum = max(result, key=result.get)  # Just use 'min' instead of 'max' for minimum.
+    
+    print(result)
+    return jsonify({"result": maximum})
 
+
+data = get_test_data()
+encoder = get_encoder()
 
 if __name__ == '__main__':
-    tokenizer_path = Path('../datasets/tokenizer.pickle').resolve()
-    with tokenizer_path.open('rb') as file:
-        tokenizer = pickle.load(file)
-
-    input_length = 100
-    model = emotionModel.emotionModel(tokenizer, input_length)
-    model_weights_path = Path('../models/emotion_recognition/model_weights.h5').resolve()
-    model.load_weights(model_weights_path.as_posix())
-
-    data = get_test_data()
-    encoder = get_encoder()
-    predict()
     app.run(debug=True)
