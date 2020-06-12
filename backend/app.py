cf493a6f4596fa079fcd27388a040059f3ae433c
BeratKARATAS53 <b21527142@cs.hacettepe.edu.tr>
2020-06-12 15:49:18 +0300

    Bug Fixed

--- backend/app.py
+++ backend/app.py
@@ -19,17 +19,12 @@ from pathlib import Path
 from keras.preprocessing.sequence import pad_sequences
 
 from emotion.pythonFiles import emotionModel
+from emotion.pythonFiles import testEmotionFlask
 from emotion.nlp import preprocess
 
 
 app = Flask(__name__)
 CORS(app)
-
-
-tokenizer_path = Path(
-    './emotion/datasets/sentiment_analysis/tokenizer.pickle').resolve()
-with tokenizer_path.open('rb') as file:
-    tokenizer = pickle.load(file)
 
 
 emoji_list = {
@@ -38,49 +33,6 @@ emoji_list = {
     "sadness": ["\ud83d\ude25"],
     "anger": ["\ud83d\udca2", "\ud83d\uddef\ufe0f", "\ud83c\udf4a"]
 }
-
-
-def string_to_pandas(sentence):
-    three = "\"\"\""
-    sentence = three + sentence + three
-    data = StringIO(sentence)
-    df = pd.read_csv(data, header=None, names=['text'])
-    return df
-
-
-def get_encoder():
-    encoder_path = Path(
-        './emotion/models/emotion_recognition/encoder.pickle').resolve()
-    with encoder_path.open('rb') as file:
-        encoder = pickle.load(file)
-    return encoder
-
-
-def preprocess_data(sentence):
-    sentence = string_to_pandas(sentence)
-    cleaned_data = preprocess(sentence.text)
-    sequences = [text.split() for text in cleaned_data]
-    list_tokenized = tokenizer.texts_to_sequences(sequences)
-    x_data = pad_sequences(list_tokenized, maxlen=100)
-    return x_data
-
-
-def predictEmotion(sentence):
-    input_length = 100
-    model = emotionModel.emotionModel(tokenizer, input_length)
-    model_weights_path = Path(
-        './emotion/models/emotion_recognition/model_weights.h5').resolve()
-    model.load_weights(model_weights_path.as_posix())
-
-    x_data = preprocess_data(sentence)
-    y_pred = model.predict(x_data)
-
-    result = {}
-    for index, value in enumerate(np.sum(y_pred, axis=0) / len(y_pred)):
-        result[encoder.classes_[index]] = value
-
-    return result
-
 
 @app.route('/')
 def home():
@@ -105,7 +57,7 @@ def home():
 @app.route('/predictModel', methods=['POST'])
 def predictModel():
     sentence = request.get_json()
-    result = predictEmotion(sentence["sentence"])
+    result = testEmotionFlask.flask_main(sentence["sentence"])
 
     maximum = max(result, key=result.get)
 
@@ -122,8 +74,6 @@ def predictModel():
 
     return jsonify(sentence)
 
-
-encoder = get_encoder()
 
 if __name__ == '__main__':
     app.run(debug=True)
